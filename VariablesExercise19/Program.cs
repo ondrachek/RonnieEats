4baee59639dd43748e725a83432acdaea4c10d64
Alexander Ondrachek <ondrachek@gmail.com>
2022-07-04 20:59:18 -0500

    initial commit

---
+++ VariablesExercise19/Program.cs
@@ -0,0 +1,12 @@
+﻿// Variable Declarations
+int age;
+string name;
+bool likesEating;
+
+//Variable Initalizations
+age = 4;
+name = "ronnie";
+likesEating = true;
+
+Console.WriteLine($"My dog's name is {name}, He is {age} years old. does he like pizza and steak and fries: {likesEating}");
+
